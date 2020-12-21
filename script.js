57c7afbc3c560009a76acd0672a661f64f69a73e
Yurii <georgesfranko@gmail.com>
2020-12-21 21:02:57 +0200

    check length email, password added

--- script.js
+++ script.js
@@ -19,9 +19,13 @@ function showSuccess(input, message){
 }
 
 // Check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())){
+        showSuccess(input);
+    }else{
+        showError(input, 'Email is not valid');
+    }
 }
 
 //Check required fields
@@ -35,16 +39,43 @@ function checkRequired(inputArr){
     });
 }
 
+// Check input length
+function checkLength(input, min, max){
+    if(input.value.length < min){
+        showError (input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if(input.value.length > max){
+        showError (input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
 //Get fieldname
 function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2){
+    if (input1.value !== input2.value){
+        showError(input2, 'Passwords do mot match');
+    } 
+
+}
+
+
+
 
 // Event listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 20);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
+
 })
 
 
